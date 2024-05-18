67e706256d2b9e58758127c9b2f0427d1de54afc
Avery Brinkman <averybrinkman@gmail.com>
2024-05-18 12:47:04 -0400

    forgot a semicolon

--- engine/src/BitBoard.cpp
+++ engine/src/BitBoard.cpp
@@ -3,7 +3,7 @@
 namespace Engine_NS {
 
 Bitboard Bitboards::getWhitePieces() const {
-  return pawns[1] | rooks[1] | knights[1] | bishops[1] | queens[1] | kings[1]
+  return pawns[1] | rooks[1] | knights[1] | bishops[1] | queens[1] | kings[1];
 }
 Bitboard Bitboards::getBlackPieces() const {
   return pawns[0] | rooks[0] | knights[0] | bishops[0] | queens[0] | kings[0];
